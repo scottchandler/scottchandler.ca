c1c826b3ad60d7f6b1dc6980a14f190af310e940
scottchandler <scott@scottchandler.ca>
2014-07-31 12:05:40 -0400

    Initial Craft install and setup.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Path to your craft/ folder
+$craftPath = './craft';
+define('CRAFT_TEMPLATES_PATH', './templates/');
+
+// Do not edit below this line
+$path = rtrim($craftPath, '/').'/app/index.php';
+
+if (!is_file($path))
+{
+	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+}
+
+require_once $path;
